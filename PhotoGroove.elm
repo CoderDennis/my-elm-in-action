35bb147c0843fce113f443973645dd07477cd9c3
CoderDennis <dodava@gmail.com>
2016-12-12 17:53:16 -0600

    end of chapter 2

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -2,20 +2,54 @@ module PhotoGroove exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
-view : a -> Html msg
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
-main : Html msg
+viewThumbnail selectedUrl thumbnail =
+    img
+        [ src (urlPrefix ++ thumbnail.url)
+        , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+        , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+update msg model =
+    if msg.operation == "SELECT_PHOTO" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
+
 main =
-    view "no model yet"
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
